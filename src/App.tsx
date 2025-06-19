8ec93e0d2fa43455710e44621f36e0b315017521
Alisya Nidila <alisyaalidin@gmail.com>
2025-06-19 17:12:23 +0800

    remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import ProfileCard from './components/ProfileCard';
 
 function App() {
-
   return (
     <div className="relative min-h-screen">
-      <img
-        src="/assets/img/Subject.png"
-        alt="Subject"
-        className="absolute left-0 bottom-0 w-32 h-auto z-10 transition-all duration-300 hover:scale-110 hover:rotate-3 hover:brightness-110 cursor-pointer"
-      />
-
       <ProfileCard />
     </div>
   );
